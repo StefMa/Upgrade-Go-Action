1440fcca9c6519331d4468665d3e5fac9fd62b9c
StefMa <StefMaDev@outlook.com>
2023-09-01 08:18:55 +0200

    Some code clean up

--- dist/index.js
+++ dist/index.js
@@ -11068,63 +11068,82 @@ const github = __nccwpck_require__(2835);
 const exec = __nccwpck_require__(3409);
 
 async function run() {
-  try {
-      let latestGoVersion;
-      const options = {};
-      options.listeners = {
-          stdout: (data) => {
-            latestGoVersion = data.toString().trim();
-          },
-      };
-      await exec.exec('go version', '', options);
-      core.info('Current installed go version: ' + latestGoVersion);
-      await exec.exec(`/bin/bash -c "go version | grep -o \\"go[0-9]\\+\\.[0-9]\\+\\" | cut -c 3-`, '', options);
+  core.info('Get current (latest) installed Go version');
+  const latestGoVersion = await getGoVersion()
 
-      core.info('Latest Go version determinate to: ' + latestGoVersion);
-      core.info('Try to modify the go.mod file with that version');
+  core.info('Try to update the "go.mod" file with Go version ' + latestGoVersion);
+  updateGoVersion(latestGoVersion)
 
-      await exec.exec('go mod edit -go=' + latestGoVersion);
-      await exec.exec('go mod tidy');
+  const changes = detectGitChanges()
+  if (changes) {
+    core.info('Changes detected. Will create a PR')
 
-      let gitChanges;
-      const options2 = {};
-      options.listeners = {
-          stdout: (data) => {
-              gitChanges += data.toString();
-          },
-      };
-      await exec.exec('git status -s', '', options2);
-      if (gitChanges != undefined || gitChanges != "") {
-          core.info('Changes detected. Will create a PR')
+    await exec.exec("git config user.name 'github-actions[bot]'");
+    await exec.exec("git config user.email 'github-actions[bot]@users.noreply.github.com'");
+    await exec.exec("git checkout -b go-upgrade-" + latestGoVersion);
+    await exec.exec(`git commit -m "Upgrade Go to version to ` + latestGoVersion + `" .`);
+    await exec.exec("git push origin go-upgrade-" + latestGoVersion);
 
-          await exec.exec("git config user.name 'github-actions[bot]'");
-          await exec.exec("git config user.email 'github-actions[bot]@users.noreply.github.com'");
-          await exec.exec("git checkout -b go-upgrade-" + latestGoVersion);
-          await exec.exec(`git commit -m "Update Go to version to ` + latestGoVersion + `" .`);
-          await exec.exec("git push origin go-upgrade-" + latestGoVersion);
+    let title = "Upgrade Go version to " + latestGoVersion
+    let head = "go-upgrade-" + latestGoVersion
+    let baseBranch = core.getInput("base-branch")
 
-          let token = process.env.GITHUB_TOKEN
-          let baseBranch = core.getInput("base-branch")
-          let head = "go-upgrade-" + latestGoVersion
-          core.info(head)
-          github.getOctokit(token).rest.pulls.create({
-              owner: github.context.repo.owner,
-              repo: github.context.repo.repo,
-              title: "Update go version to " + latestGoVersion,
-              body: "",
-              head: head,
-              base: baseBranch
-          });
-      } else {
-          core.info('Seems everything is up to date 🎉');
-      }
+    let token = process.env.GITHUB_TOKEN
+    let pullCreateResponse = github.getOctokit(token).rest.pulls.create({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      title: title,
+      body: "",
+      head: head,
+      base: baseBranch
+    });
+    let prUrl = (await pullCreateResponse).data.html_url
 
-    } catch (error) {
-      core.setFailed(error.message);
-    }
+    core.info('PR created. Check it out at ' + prUrl);
+  } else {
+    core.info('Seems everything is up to date 🎉');
+  }
 }
 
-run();
+async function getGoVersion() {
+  let latestGoVersion;
+  const execOptionsLatesGoVersion = {};
+  execOptionsLatesGoVersion.listeners = {
+    stdout: (data) => {
+      latestGoVersion = data.toString().trim();
+    },
+  };
+  await exec.exec('go version', '', execOptionsLatesGoVersion);
+  await exec.exec(`/bin/bash -c "go version | grep -o \\"go[0-9]\\+\\.[0-9]\\+\\" | cut -c 3-`, '', execOptionsLatesGoVersion);
+
+  return latestGoVersion
+}
+
+async function updateGoVersion(goVersion) {
+  await exec.exec('go mod edit -go=' + goVersion);
+  await exec.exec('go mod tidy');
+}
+
+async function detectGitChanges() {
+  let gitChanges;
+  const execOptionsGitChanges = {};
+  execOptionsGitChanges.listeners = {
+    stdout: (data) => {
+      gitChanges += data.toString();
+    },
+  };
+  await exec.exec('git status -s', '', execOptionsGitChanges);
+  return gitChanges != undefined || gitChanges != ""
+}
+
+run()
+  .then(result => {
+    console.log('🟢  Done 🎉');
+  })
+  .catch(error => {
+    console.error(`🔴 Error: ${error.message}`);
+    core.setFailed(`Action failed with error: ${error.message}`);
+  });
 })();
 
 module.exports = __webpack_exports__;
