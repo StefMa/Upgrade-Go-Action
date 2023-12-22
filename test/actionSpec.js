2eadd1ec03b8b05bd891e163147b0dc5351e8384
StefMa <StefMaDev@outlook.com>
2023-12-22 13:18:59 +0100

    Avoid creating a PR in case branch already exist

--- test/actionSpec.js
+++ test/actionSpec.js
@@ -1,4 +1,4 @@
-const { getGoVersion } = require('../action.js');
+const { getGoVersion, branchWithNameAlreadyExist } = require('../action.js');
 const assert = require('assert');
 
 describe('Action', function () {
@@ -7,5 +7,29 @@ describe('Action', function () {
       const goVersion = await getGoVersion()
       assert.equal(goVersion, "1.21")
     });
+  });
+
+  describe('#branchWithNameAlreadyExist()', function () {
+    it('should return false because branch does not exist', async function () {
+      let token = process.env.GITHUB_TOKEN
+      const exist = await branchWithNameAlreadyExist(
+        "branchWillNeverExists!Hopefully", 
+        token,
+        "StefMa",
+        "Upgrade-Go-Action"
+      )
+      assert.equal(exist, false)
+    });
+  });
+
+  it('should return true because main branch exist', async function () {
+    let token = process.env.GITHUB_TOKEN
+    const exist = await branchWithNameAlreadyExist(
+      "main", 
+      token,
+      "StefMa",
+      "Upgrade-Go-Action"
+    )
+    assert.equal(exist, true)
   });
 });
