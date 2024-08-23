38365603090d8f3d5ea7fc6cbbb1fd1b93d0c75c
Stefan M. <StefMa@users.noreply.github.com>
2024-08-23 11:09:42 +0000

    Fix test name

--- test/actionSpec.js
+++ test/actionSpec.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 
 describe('Action', function () {
   describe('#getGoVersion()', function () {
-    it('should return 1.21', async function () {
+    it('should return 1.23.0', async function () {
       const goVersion = await getGoVersion()
       assert.equal(goVersion, "1.23.0")
     });
